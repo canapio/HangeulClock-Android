e69b86dd254b7752f9705352cd85868cf91e957e
Sean <seanoh1989@gmail.com>
2015-11-18 22:30:46 -0800

    commit before merge

--- Android/Hansi/app/src/main/java/com/hangulclock/hansi/ColorActivity.java
+++ Android/Hansi/app/src/main/java/com/hangulclock/hansi/ColorActivity.java
@@ -9,6 +9,8 @@ public class ColorActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_color);
+
+
     }
 
 }
